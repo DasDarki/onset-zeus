a7a8a3a16faeacbdd11db5df2a9603a49be710dd
DasDarki <rageskilln@gmail.com>
2020-01-06 12:36:29 +0100

    Correct Menu sizing

--- client/index.lua
+++ client/index.lua
@@ -14,9 +14,10 @@ local function OpenUI()
 end
 
 function OnPackageStart()
-    local width, height = GetScreenSize()
-    web_ui = CreateWebUI(0, 0, width, height, 1, 60)
+    web_ui = CreateWebUI(0, 0, 0, 0, 1, 60)
     LoadWebFile(web_ui, "http://asset/" .. GetPackageName() .. "/client/ui/index.html")
+    SetWebAlignment(web_ui, 0.0, 0.0)
+    SetWebAnchors(web_ui, 0.0, 0.0, 1.0, 1.0)
     SetWebVisibility(web_ui, WEB_HIDDEN)
 end
 AddEvent("OnPackageStart", OnPackageStart)
